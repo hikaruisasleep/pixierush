ce932b66d87d3acb00b9512a4347330c56e5785f
Ben Wu <ben@jxcb.xyz>
2025-03-10 15:10:13 +0700

    dfjdkjf

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import "../app.css";
+  let { children } = $props();
+</script>
+
+<div class="min-h-screen md:max-w-md max-w-screen flex flex-col flex-0 mx-auto">
+  {@render children()}
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,113 @@
+<script lang="ts">
+  import pixiepad from "$lib/pixiepad.png";
+  import bg_pixelcity_vp9 from "$lib/bg-pixelcity-vp9.webm";
+  import bg_pixelcity_av1 from "$lib/bg-pixelcity-av1.webm";
+  import { onMount } from "svelte";
+
+  function report() {
+    console.log(`mask ${maskStates}`);
+  }
+
+  let maskStates: [number, boolean] = $state([0, true]);
+
+  function switchMasks() {
+    if (maskStates[1] == true) {
+      maskStates[1] = false;
+    } else {
+      if (maskStates[0] >= 2) {
+        maskStates[0] = 0;
+      } else {
+        maskStates[0] = maskStates[0] + 1;
+      }
+      maskStates[1] = true;
+    }
+  }
+
+  onMount(() => {
+    const interval = setInterval(() => switchMasks(), 2000);
+    return () => {
+      clearInterval(interval);
+    };
+  });
+</script>
+
+<header class="w-full py-4 px-12 flex flex-row justify-center shadow-gray-400 shadow-md fixed top-0 right-0 left-0 bg-white z-[1]">
+  <div class="logo flex flex-col items-center justify-center gap-1">
+    <div class="max-w-16 sm:max-w-10">
+      <img src={pixiepad} alt="pixierush logo" />
+    </div>
+    <h1 class="text-lg sm:text-xs">pIxIerush.Id</h1>
+  </div>
+</header>
+
+<nav class="nav flex flex-row items-center justify-around gap-1 hidden">
+  <a href="">home</a>
+  <a href="">about</a>
+  <a href="">about</a>
+</nav>
+
+<div class="h-screen w-full py-10 flex flex-col items-center justify-center relative">
+  <video autoplay muted loop playsinline class="fixed top-0 -z-50 h-full w-full md:w-md object-cover">
+    <source src={bg_pixelcity_av1} type="video/webm" />
+    <source src={bg_pixelcity_vp9} type="video/webm" />
+  </video>
+  <div class="bg-gradient-to-b from-transparent from-0% via-white/50 via-50% to-transparent to-100% w-full h-1/2 flex flex-col justify-center items-center">
+    <h2 class="w-full text-fulfill leading-none inline-block px-2 select-none">
+      <span
+        ><span>
+          Your gaming<br />
+          <span class="text-[#cb1643] bg-white absolute" class:hidden={maskStates[0] != 0} class:mask-item-in={maskStates[1]} class:mask-item-out={!maskStates[1]}>
+            friend.
+          </span>
+          <span class="text-[#cb1643] bg-white absolute" class:hidden={maskStates[0] != 1} class:mask-item-in={maskStates[1]} class:mask-item-out={!maskStates[1]}>
+            curator.
+          </span>
+          <span class="text-[#cb1643] bg-white absolute" class:hidden={maskStates[0] != 2} class:mask-item-in={maskStates[1]} class:mask-item-out={!maskStates[1]}>
+            companion.
+          </span>
+        </span></span
+      >
+      <span aria-hidden="true">Your gaming<br />companion.</span>
+    </h2>
+    <p class="text-white font-[NHAAS] tracking-wider relative top-54">recommendations, reviews, and more</p>
+  </div>
+
+  <div class="absolute bottom-12">
+    <span class="text-white static text-2xl">
+      <i class="fa-solid fa-chevron-down absolute bottom-2 -left-[0.9ch]"></i>
+      <i class="fa-solid fa-chevron-down absolute bottom-0 -left-[0.9ch]"></i>
+    </span>
+  </div>
+</div>
+
+<div class="h-screen flex bg-black"></div>
+
+<style>
+  .mask-item-in {
+    animation: mask-in 2s ease 1 normal;
+    animation-fill-mode: backwards;
+  }
+
+  .mask-item-out {
+    animation: mask-out 2s ease 1 normal;
+    animation-fill-mode: forwards;
+  }
+
+  @keyframes mask-in {
+    0% {
+      mask: linear-gradient(90deg, #000 50%, #0000 0) content-box 100% 100% / 200% 200% no-repeat;
+    }
+    100% {
+      mask: linear-gradient(90deg, #000 50%, #0000 0) content-box 0% 0% / 200% 200% no-repeat;
+    }
+  }
+
+  @keyframes mask-out {
+    0% {
+      mask: linear-gradient(270deg, #000 50%, #0000 0) content-box 100% 100% / 200% 200% no-repeat;
+    }
+    100% {
+      mask: linear-gradient(270deg, #000 50%, #0000 0) content-box 0% 0% / 200% 200% no-repeat;
+    }
+  }
+</style>
